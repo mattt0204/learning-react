1b3e7c2beda45e080100a04e9d23a8430cf028f4
Neo0204 <wootaejung24@gmail.com>
2020-02-22 22:40:49 +0900

    3,4 Component and EventHandling

---
+++ 3-7-component/src/Counter.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+      fixedNumber: 0
+    };
+  }
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
+  render() {
+    const { number, fixedNumber } = this.state;
+
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h1>바뀌지 않는 값 {fixedNumber}</h1>
+        <button
+          onClick={() => {
+            this.setState(prevState => {
+              return {
+                number: prevState.number + 1
+              };
+            });
+
+            this.setState(prevState => ({
+              number: prevState.number + 1
+            }));
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ 3-7-component/src/EventPractice.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const EventPractice = () => {
+  const [form, setForm] = useState({ username: '', message: '' });
+
+  const onChange = e => {
+    const nextForm = { ...form, [e.target.name]: e.target.value };
+    setForm(nextForm);
+  };
+  const { username, message } = form;
+
+  const handleClick = () => {
+    alert(form.username + ':' + form.message);
+    setForm({ username: '', message: '' });
+  };
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        placeholder="유저를 입력하세요"
+        name="username"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="메세지를 입력하세요"
+        name="message"
+        value={message} // value에 들어가는 이 컴포넌트 상태의 값이지  e.target.value가 아니다.
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+
+      <button onClick={handleClick}>Click</button>
+    </div>
+  );
+};
+
+export default EventPractice;
